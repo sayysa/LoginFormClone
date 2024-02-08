1eee647dc20336d9e4cfa540eb6d40f7aa79e1ba
ysa <nybfarofaldane@gmail.com>
2024-02-08 18:22:12 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import logo from './Assets/github-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <form>
+        <div className="icon">
+          {/* <img src={} alt="GitHub Icon" /> */}
+        </div>
+        <h1>Sign in to GitHub</h1>
+
+        <div className="body-container">
+          <div className="input-box">
+            <label>Username or email address</label>
+            <br />
+            <input type="text" required />
+          </div>
+
+          <div className="input-box">
+          <div class="label">
+            <label>Password
+            <a href="#" className="forgot-password">Forgot Password?</a>
+            </label>
+            <br />
+            <input type="password" required />
+          </div>
+          </div>
+
+          <input type="submit" class="btn" value="Sign in"></input>
+        </div>
+      </form>
+
+      <div class="login-callout">
+            <p class="#">New to GitHub?
+              <a href="#" className="create">Create an account</a>
+            </p>
+      </div>
+    
+      <footer>
+        <span> Terms </span>
+        <span> Privacy </span>
+        <span> Docs </span>
+        <span> Contact GitHub Support </span>
+        <span> Manage cookies </span>
+        <span> Donut share my personal information </span>
+      </footer>
+
     </div>
   );
 }
